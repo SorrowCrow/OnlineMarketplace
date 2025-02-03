e66f7e23bcab776b861353de964b1525428ec0a4
SorrowCrow <78910309+SorrowCrow@users.noreply.github.com>
2025-02-03 15:40:59 +0200

    Initial commit

---
+++ src/test/java/com/OnlineMarketplace/OnlineMarketplace/OnlineMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OnlineMarketplace.OnlineMarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
