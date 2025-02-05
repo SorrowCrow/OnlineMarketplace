bd1dc5aa5639ee67b328e24b4ab9f70e7aab7493
SorrowCrow <78910309+SorrowCrow@users.noreply.github.com>
2025-02-05 09:23:52 +0200

    fix: fixed Category to use lombok

--- src/main/java/com/OnlineMarketplace/OnlineMarketplace/category/Category.java
+++ src/main/java/com/OnlineMarketplace/OnlineMarketplace/category/Category.java
@@ -1,9 +1,17 @@
 package com.OnlineMarketplace.OnlineMarketplace.category;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "categories")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Category {
 
     @Id
@@ -15,38 +23,6 @@ public class Category {
 
     @Column(length = 500)
     private String description;
-
-    public Category() {}
-
-    public Category(Long id, String name, String description) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
 
 
